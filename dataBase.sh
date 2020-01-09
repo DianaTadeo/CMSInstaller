f16b83b20d15b151feb8656533da18f949fddc41
root <root@deb9proy.proyecto>
2020-01-08 19:00:52 -0600

    terminada la creacion de base de datos

--- dataBase.sh
+++ dataBase.sh
@@ -12,5 +12,14 @@ then
 	#su postgresql -c "psql -c 'GRANT ALL PRIVILEGES ON DATABASE $baseName TO b$userName;'"
 else
 	echo "++++ Instalando MySQL/MariaDB ++++"
-	apt-get install mariadb-server
+	apt-get install mariadb-server -y
+	echo "++++ Se ha instalado correctamente ++++"
 	mysql_secure_installation
+	read -p "Ingresa el nombre de la base de datos: " baseName
+	#mysql -e "CREATE DATABASE $baseName;"
+	read -p "In gresa el nombre de usuario administrador de la base de datos: " userName
+	read -sp "Ingresa el password para ese usuario: " userPass
+	#mysql -e "CREATE USER $userName IDENTIFIED BY $userPass;"
+	#mysql -e "GRANT ALL PRIVILEGES ON $baseName TO $userName;"
+	#mysql -e "FLUSH PRIVILEGES;"
+fi
