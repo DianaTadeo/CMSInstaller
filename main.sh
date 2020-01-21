648ea8fd102bc4072a6f43864e6514d1aa307bd7
RafaelVallejo <rafael.vallejo@bec.seguridad.unam.mx>
2020-01-21 12:47:15 -0600

    Función para hardening, ipv4_ipv6 y shebang modificado

--- main.sh
+++ main.sh
@@ -1,10 +1,30 @@
-#!/bin/bash
+#!/bin/sh
 
+####################################################################
+# Script 	Main de instalador y configurador de CMS seguros
+# en Debian 9, 10 y CentOS 6, 7
+
+# Argumento 1: fileID.json generado desde el sitio web
+LOG="`pwd`/../Log/Configuracion_Instalacion_CMS.log"
+
+
+###################### Log de Errores ###########################
+# $1: Salida de error											#
+# $2: Mensaje de la instalacion									#
+#################################################################
+log_errors(){
+	if [ $1 -ne 0 ]; then
+		echo "[`date +"%F %X"`] : $2 : [ERROR]" >> $LOG
+		exit 1
+	else
+		echo "[`date +"%F %X"`] : $2 : [OK]" 	>> $LOG
+	fi
+}
 jq_install_OS_detection(){
 	# Función que instala jq detectando el SO (Debian o CentOS)
-	if [[ `cat /etc/issue | grep -E 'Debian'| wc -l` == '1' ]]; then
+	if [ `cat /etc/issue | grep -E 'Debian'| wc -l` == '1' ]; then
 		apt install jq -y
-	elif [[ -e "/etc/centos-release" ]]; then
+	elif [ -e "/etc/centos-release" ]; then
 		yum install epel-release -y
 		yum install jq -y
 # Para script final se habilita exit
@@ -27,13 +47,27 @@ OS_dependencies(){
 	esac
 }
 
+ip_v4_v6(){
+	# Función para habilitar soporte de IPv4 e IPv6
+	# $1=IPv4 $2=IPv6
+	if [ $1 = "Yes" ]; then	IPV4_SUPPORT="0"; else IPV4_SUPPORT="1";	fi
+	sysctl -w net.ipv4.conf.all.disable_ipv4=$IPV4_SUPPORT
+	sysctl -w net.ipv4.conf.default.disable_ipv4=$IPV4_SUPPORT
+	log_errors $? "Soporte para IPv4: $1"
+	if [ $2 = "Yes" ]; then	IPV6_SUPPORT="0"; else IPV6_SUPPORT="1";	fi
+	sysctl -w net.ipv6.conf.all.disable_ipv6=$IPV6_SUPPORT
+	sysctl -w net.ipv6.conf.default.disable_ipv6=$IPV6_SUPPORT
+	log_errors $? "Soporte para IPv6: $2"
+}
+
 OS_hardening(){
 	# $1=SO; $2=EMAIL_NOTIFICATION
 	echo "TODO"
-	# configuraciones generales de hardening (política de contraseñas, sudo, servicios predet., etc.)
-	# ./Modulos/Hardening/Configuraciones_Generales.sh "$1"
 	# Instalación y configuración de F2Ban, logwatch y logcheck
-	# bash ./Modulos/Hardening/F2BanLogwatchLogcheck.sh "$1" "$2"
+	bash ./Modulos/Hardening/F2BanLogwatchLogcheck.sh "$1" "$2"
+
+	 # configuraciones generales de hardening (política de contraseñas, sudo, servicios predet., etc.)
+	bash ./Modulos/Hardening/Configuraciones_Generales.sh "$1"
 }
 
 web_server_installer(){
@@ -42,11 +76,11 @@ web_server_installer(){
 	case $1 in
 		'Debian 9' | 'Debian 10')
 			# Se ejecuta script para instalación de web server en debian
-			 ./Modulos/Auxiliares/Apache_Nginx_Debian.sh "$1" "$2" "$3"
+			 bash ./Modulos/Auxiliares/Apache_Nginx_Debian.sh "$1" "$2" "$3"
 		;;
 		'CentOS 6' | 'CentOS 7')
 			# Se ejecuta script para instalación de web server en centos
-			 ./Modulos/Auxiliares/Apache_Nginx_CentOS.sh "$1" "$2" "$3"
+			 bash ./Modulos/Auxiliares/Apache_Nginx_CentOS.sh "$1" "$2" "$3"
 		;;
 	esac
 }
@@ -104,7 +138,7 @@ backups(){
 #																																					#
 ###########################################################################
 
-if [[ -z $(which sudo) ]]; then
+if [ -z $(which sudo) ]; then
 	echo "Para ejecutar el script primero se instalará sudo:"
 	case $SO in
 		'Debian 9' | 'Debian 10')
@@ -118,7 +152,7 @@ if [[ -z $(which sudo) ]]; then
 	exit 1
 fi
 
-if [ $(id -u) -ne 0 ] && [[ -z "$SUDO_USER" ]];then
+if [ $(id -u) -ne 0 ] && [ -z "$SUDO_USER" ];then
 	echo "Ejecución: sudo ./main.sh"
 	exit 1
 fi
@@ -155,12 +189,13 @@ BACKUP_TIME=`jq '.BackupTime' $JSON_OPTIONS | cut -f2 -d'"'`
 DB_EXISTS=`jq '.DBExists' $JSON_OPTIONS | cut -f2 -d'"'`
 
 # Se ejecutan las funciones para realizar las instalaciones y configuraciones
+
 OS_dependencies "$SO"
 chmod +x ./Modulos/Auxiliares/* ./Modulos/InstaladoresCMS/* ./Modulos/Hardening
 web_server_installer "$SO" "$WEB_SERVER" "$WS_VERSION"
 
 # Se asginan valores para conexión a la BD si existe o no
-if [ $DB_EXISTS == "Yes" ]; then
+if [ $DB_EXISTS = "Yes" ]; then
 	DB_USER=`jq '.DBUser' $JSON_OPTIONS | cut -f2 -d'"'`
 	DB_IP=`jq '.DBIP' $JSON_OPTIONS | cut -f2 -d'"'`
 	DB_PORT=`jq '.DBPort' $JSON_OPTIONS | cut -f2 -d'"'`
