43c0ce2f3f6ce213f848f00241318905f72672ee
RafaelVallejo <rafael.vallejo@bec.seguridad.unam.mx>
2020-01-25 03:36:39 -0600

    Se realiza prueba a conexión de BD remota

--- Modulos/Auxiliares/DB_Instalador_Debian.sh
+++ Modulos/Auxiliares/DB_Instalador_Debian.sh
@@ -22,7 +22,7 @@ LOG="`pwd`/Modulos/Log/Aux_Instalacion.log"
 log_errors(){
 	if [ $1 -ne 0 ]; then
 		echo "[`date +"%F %X"`] : $2 : [ERROR]" >> $LOG
-		exit_install
+		exit 1
 	else
 		echo "[`date +"%F %X"`] : $2 : [OK]" 	>> $LOG
 	fi
@@ -40,18 +40,18 @@ install_MySQL(){
 	log_errors $? "Instalacion de MySQL: $cmd"
 	read -sp "Ingresa el password para el usuario '$4': " userPass; echo -e "\n"
 	if [[ $2 == 'Yes' ]]; then
-		mysql -h $5 -P $6 -u $4 --password=$userPass $3
+		mysql -h $5 -P $6 -u $4 --password=$userPass -e "\q"
 		log_errors $? "Conexión a la base de datos '$3' en MySQL, servidor $5"
 	else
 		read -sp "Ingresa el password para el usuario 'root' de MySQL: " rootPass; echo -e "\n"
 		mysql -h $5 -P $6 -u 'root' --password=$rootPass -e "CREATE USER '$4' IDENTIFIED BY '$userPass';"
-		log_errors $? "Creación del usuario '$4' en MySQL, servidor $5"
+		log_errors $? "Creación del usuario '$4' en MySQL, servidor '$5'"
 		mysql -h $5 -P $6 -u 'root' --password=$rootPass -e "GRANT ALL PRIVILEGES ON *.* TO '$4';"
-		log_errors $? "Permisos otorgador al usuario '$4' en MySQL, servidor $5"
+		log_errors $? "Permisos otorgador al usuario '$4' en MySQL, servidor '$5'"
 		mysql -h $5 -P $6 -u 'root' --password=$rootPass -e "CREATE DATABASE $3;"
-		log_errors $? "Creación de la base de datos '$3' en MySQL, servidor $5"
+		log_errors $? "Creación de la base de datos '$3' en MySQL, servidor '$5'"
 		mysql -h $5 -P $6 -u 'root' --password=$rootPass -e "FLUSH PRIVILEGES;"
-		log_errors $? "Privilegios otorgados al usuario '$4' en MySQL, servidor $5"
+		log_errors $? "Privilegios otorgados al usuario '$4' en MySQL, servidor '$5'"
 	fi
 }
 
@@ -62,16 +62,17 @@ install_PostregSQL(){
 	log_errors $? "Instalacion de PostgreSQL: $cmd"
 	if [[ $2 == 'Yes' ]]; then
 		# solamente hace conexión a la BD existente
-		su postgres -c "psql -h $5 -p $6 -d $3 -U $4"
-		log_errors $? "Error al conectarse a la base de datos $3 en PostgreSQL, servidor $5"
+		su postgres -c "psql -h $5 -p $6 -d $3 -U $4 -c '\q'"
+		log_errors $? "Conexión a la base de datos '$3' en PostgreSQL, servidor $5"
 	else
+		read -sp "Ingresa el password para el usuario '$4': " userPass; echo -e "\n"
 		su postgres -c "psql -h $5 -p $6 -c 'CREATE DATABASE $3;'"
-		log_errors $? "Error al crear la base de datos $3 en PostgreSQL, servidor $5"
-		read -s "Ingresa el password para ese usuario: " userPass
-		su -c "psql -h $5 -p $6 -c \"CREATE USER $4 WITH PASSWORD '$userPass'\" " postgres
-		log_errors $? "Error al crear al usuario $4 en PostgreSQL, servidor $5"
+		log_errors $? "Creación de la base de datos $3 en PostgreSQL, servidor '$5'"
+		su -c "psql -h $5 -p $6 -c \"CREATE USER $4 WITH PASSWORD '$userPass';\" " postgres
+		log_errors $? "Creación del usuario '$4' en PostgreSQL, servidor '$5'"
 		su postgres -c "psql -h $5 -p $6 -c 'GRANT ALL PRIVILEGES ON DATABASE $3 TO $4;'"
-		log_errors $? "Error en privilegios para el usuario $4 en PostgreSQL, servidor $5"
+		log_errors $? "Privilegios otorgador al usuario '$4' en PostgreSQL, servidor '$5'"
+
 	fi
 }
 
