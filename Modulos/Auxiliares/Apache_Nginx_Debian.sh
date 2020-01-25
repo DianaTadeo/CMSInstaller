683887f95329af768f42fc0c5d712be515cfbc32
RafaelVallejo <rafael.vallejo@bec.seguridad.unam.mx>
2020-01-25 03:41:19 -0600

    Se eliminó última línea

--- Modulos/Auxiliares/Apache_Nginx_Debian.sh
+++ Modulos/Auxiliares/Apache_Nginx_Debian.sh
@@ -228,4 +228,3 @@ else
 	install_apache_WAF
 fi
 chown -R www-data:www-data /var/www/html
-#apt-get install  php7.4-intl php7.4-mysql php7.4-curl php7.4-gd php7.4-soap php7.4-xml php7.4-zip php7.4-readline php7.4-opcache php7.4-json php7.4-gd -y apt-get
