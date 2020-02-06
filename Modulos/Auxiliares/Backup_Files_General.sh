8938253e763641002ebad7e43e4d48ba8dad1338
RafaelVallejo <rafael.vallejo@bec.seguridad.unam.mx>
2020-02-06 00:26:00 -0600

    Se agrego respaldo de VH, y se crea script para cron que realiza los respaldos con el formato correcto

--- Modulos/Auxiliares/Backup_Files_General.sh
+++ Modulos/Auxiliares/Backup_Files_General.sh
@@ -61,12 +61,14 @@ backups(){
 		systemctl start cron
 		if [[ $3 == "Apache" ]]; then
 			WEB_CONFIG="/etc/apache2/apache2.conf"
-			SECURITY_CONF="/etc/apache2/conf-enabled/security.conf"
+			SECURITY_CONF="/etc/apache2/conf-available/security.conf"
+			VH_CONFIG="/etc/apache2/sites-available/$6.conf"
 			WAF_CONFIG="/etc/modsecurity/modsecurity.conf"
 			WAF_CONFIG_MOD="/etc/apache2/mods-enabled/security2.conf"
 		else
 			WEB_CONFIG="/usr/local/nginx/conf/nginx.conf"
 			SECURITY_CONF=""
+			VH_CONFIG="/etc/....../$6.conf"
 			WAF_CONFIG="/usr/local/nginx/conf/modsecurity.conf"
 			WAF_CONFIG_MOD="/usr/local/nginx/conf/crs-setup.conf"
 		fi
@@ -75,8 +77,10 @@ backups(){
 		else
 			if [[ $2 == "Debian 9" ]]; then
 				DBM_CONFIG="/etc/postgresql/9.6/main/postgresql.conf"
+				DBM_CONF="/etc/postgresql/9.6/main/pg_hba.conf"
 			else
 				DBM_CONFIG="/etc/postgresql/11/main/postgresql.conf"
+				DBM_CONF="/etc/postgresql/11/main/pg_hba.conf"
 			fi
 		fi
 	else
@@ -84,12 +88,14 @@ backups(){
 		if [[ $3 == "Apache" ]]; then
 			WEB_CONFIG="/etc/httpd/conf/httpd.conf"
 			SECURITY_CONF="/etc/httpd/conf.d/security.conf"
+			VH_CONFIG="/etc/httpd/sites-available/$6.conf"
 			WAF_CONFIG="/etc/httpd/conf.d/mod_security.conf"
 			WAF_CONFIG_MOD=""
 
 		else
 			WEB_CONFIG=""
 			SECURITY_CONF=""
+			VH_CONFIG="/.../$6.conf"
 			WAF_CONFIG=""
 			WAF_CONFIG_MOD=""
 
@@ -98,61 +104,96 @@ backups(){
 			DBM_CONFIG="/etc/my.cnf"
 		else
 			DBM_CONFIG="/var/lib/pgsql/data/"
+			DBM_CONF="/var/lib/pgsql/data/pg_hba.conf"
 		fi
 	fi
 	FIREWALL_CONFIG="./Modulos/Auxiliares/firewall/iptables.v4"
 
-	HOUR=$(echo ${11} | cut -f1 -d":")
-	MIN=$(echo ${11} | cut -f2 -d":")
-	CRONTAB="/etc/crontab"
-	# hour		min		day_of_month		month		day_of_week		user command_to_be_executed
+	CRON_SCRIPT="/usr/bin/backup_cron.sh"
+	echo '#!/bin/bash' > $CRON_SCRIPT
+	echo -e '\nDATE=$(date "+%F")' >> $CRON_SCRIPT
 
 	mkdir backup
 	cd backup
-	# Respaldo de configuración Web
-	tar -czvf config_files_web.tar.gz $WEB_CONFIG $SECURITY_CONF
-	log_errors $? "Respaldos de configuraciones web actuales: config_files_web.tar.gz"
-	echo "$MIN $HOUR		*	*	$1 		root		tar -czvf $PWD/config_files_web`date +"%F"`.tar.gz $WEB_CONFIG $SECURITY_CONF" >> $CRONTAB
-	log_errors $? "Respaldos de configuraciones web programada: $1 on $HOUR:$MIN"
+	CURR_DIR=$PWD
+	chown $SUDO_USER:$SUDO_USER $CURR_DIR -R
+	DATE=$(date "+%F")
+	log_errors $? "Se crea directorio de respaldos en: $CURR_DIR"
+	# Respaldo de configuraciones Web
+	tar -czvf "$CURR_DIR/$DATE-config_files_web.tar.gz" \
+	-C $(dirname $WEB_CONFIG) $(basename $WEB_CONFIG) \
+	-C  $(dirname $VH_CONFIG) $(basename $VH_CONFIG) \
+	-C $(dirname $SECURITY_CONF) $(basename $SECURITY_CONF)
+	log_errors $? "Respaldos de configuraciones web actuales: $DATE-config_files_web.tar.gz"
+	echo "tar -czvf "$CURR_DIR/'$DATE'-config_files_web.tar.gz" \
+	-C $(dirname $WEB_CONFIG) $(basename $WEB_CONFIG) \
+	-C $(dirname $VH_CONFIG) $(basename $VH_CONFIG) \
+	-C $(dirname $SECURITY_CONF) $(basename $SECURITY_CONF)" >> $CRON_SCRIPT
+	log_errors $? "Programación de respaldos de configuraciones web agregada en '$CRON_SCRIPT'"
+
 	# Respaldo de config de BD
-	tar -czvf config_files_db.tar.gz $DBM_CONFIG
-	log_errors $? "Respaldos de configuraciones de BD actuales: config_files_db.tar.gz"
-	echo "$MIN $HOUR		*	*	$1 		root		tar -czvf $PWD/config_files_db`date +"%F"`.tar.gz $DBM_CONFIG" >> $CRONTAB
-	log_errors $? "Respaldos de configuraciones de BD programado: $1 on $HOUR:$MIN"
+	[[ $4 == "PostgreSQL" ]] && CONF_PSQL="-C $(dirname $DBM_CONF) $(basename $DBM_CONF)"
+	tar -czvf "$CURR_DIR/$DATE-config_files_db.tar.gz" \
+	-C $(dirname $DBM_CONFIG) $(basename $DBM_CONFIG) $CONF_PSQL
+	log_errors $? "Respaldos de configuraciones de BD actuales: $DATE-config_files_db.tar.gz"
+	echo "tar -czvf "$CURR_DIR/'$DATE'-config_files_db.tar.gz" \
+	-C $(dirname $DBM_CONFIG) $(basename $DBM_CONFIG) $CONF_PSQL" >> $CRON_SCRIPT
+	log_errors $? "Programación de respaldos de configuraciones de BD agregada en '$CRON_SCRIPT'"
 
 	# Respaldo de config de WAF
-	tar -czvf config_files_waf.tar.gz $WAF_CONFIG $WAF_CONFIG_MOD
-	log_errors $? "Respaldo de configuraciones WAF actuales: config_files_waf.tar.gz"
-	echo "$MIN $HOUR		*	*	$1 		root		tar -czvf $PWD/config_files_waf`date +"%F"`.tar.gz $WAF_CONFIG $WAF_CONFIG_MOD" >> $CRONTAB
-	log_errors $? "Respaldo de configuraciones de WAF programado: $1 on $HOUR:$MIN"
+	tar -czvf "$CURR_DIR/$DATE-config_files_waf.tar.gz" \
+	-C $(dirname $WAF_CONFIG) $(basename $WAF_CONFIG) \
+	-C $(dirname $WAF_CONFIG_MOD) $(basename $WAF_CONFIG_MOD)
+	log_errors $? "Respaldo de configuraciones WAF actuales: $DATE-config_files_waf.tar.gz"
+	echo "tar -czvf "$CURR_DIR/'$DATE'-config_files_waf.tar.gz" \
+	-C $(dirname $WAF_CONFIG) $(basename $WAF_CONFIG) \
+	-C $(dirname $WAF_CONFIG_MOD) $(basename $WAF_CONFIG_MOD)" >> $CRON_SCRIPT
+	log_errors $? "Programación de respaldos de WAF de BD agregada en '$CRON_SCRIPT'"
 
 	# Respaldo de config de Firewall
 	if [[ -e $FIREWALL_CONFIG ]]; then
-		tar -czvf config_files_firewall.tar.gz $FIREWALL_CONFIG
-		log_errors $? "Respaldo de configuraciones de firewall actuales: config_files_firewall.tar.gz"
-		echo "$MIN $HOUR		*	*	$1 		root		tar -czvf $PWD/config_files_firewall`date +"%F"`.tar.gz $FIREWALL_CONFIG" >> $CRONTAB
-		log_errors $? "Respaldo de configuraciones de firewall programado: $1 on $HOUR:$MIN"
+		tar -czvf "$CURR_DIR/$DATE-config_files_firewall.tar.gz" \
+		-C $(dirname $FIREWALL_CONFIG) $(basename $FIREWALL_CONFIG)
+		log_errors $? "Respaldo de configuraciones de firewall actuales: $DATE-config_files_firewall.tar.gz"
+		echo "tar -czvf "$CURR_DIR/'$DATE'-config_files_firewall.tar.gz" \
+		-C $(dirname $FIREWALL_CONFIG) $(basename $FIREWALL_CONFIG)" >> $CRON_SCRIPT
+		log_errors $? "Programación de respaldos de firewall agregada en '$CRON_SCRIPT'"
 	fi
 	echo "Respaldo de datos:"
 	# Respaldo sitio web
-	tar -czf $6.tar.gz $5/$6
-	log_errors $? "Respaldo de datos de sitio web actual: $6.tar.gz"
-	echo "$MIN $HOUR		*	*	$1 		root		tar -czf $PWD/$6`date +"%F"`.tar.gz $5/$6" >> $CRONTAB
-	log_errors $? "Respaldo de datos de sitio web programado: $1 on $HOUR:$MIN"
+	tar -czf "$CURR_DIR/$DATE-$6.tar.gz" \
+	-C $5 $6
+	log_errors $? "Respaldo de datos de sitio web actual: $DATE-$6.tar.gz"
+	echo "tar -czvf $CURR_DIR/'$DATE'-$6.tar.gz \
+	-C $5 $6" >> $CRON_SCRIPT
+	log_errors $? "Programación de respaldo de datos de sitio web agregada en '$CRON_SCRIPT'"
 
 	# Respaldo de BD
 	read -sp "Ingresa la contraseña del usuario '$7' de la BD para hacer el respaldo: " DB_PASS; echo -e "\n"
 	if [[ $4 == "MySQL" ]]; then
-		mysqldump -u $7 --password=$DB_PASS -h $8 -P $9 ${10} > ${10}.sql
-		log_errors $? "Respaldo de BD actual: ${10}.sql"
-		echo "$MIN $HOUR		*	*	$1 		root		mysqldump -u $7 --password=$DB_PASS -h $8 -P $9 ${10} > $PWD/${10}`date +"%F"`.sql" >> $CRONTAB
-		log_errors $? "Respaldo de BD programado: $1 on $HOUR:$MIN"
+		mysqldump -u $7 --password=$DB_PASS -h $8 -P $9 ${10} > "$DATE-${10}.sql"
+		log_errors $? "Respaldo de BD actual: $DATE-${10}.sql"
+		echo "mysqldump -u $7 --password=$DB_PASS -h $8 -P $9 ${10} > '$DATE'-${10}.sql" >> $CRON_SCRIPT
+		log_errors $? "Programación de respaldo de BD agregada en '$CRON_SCRIPT'"
 	else
-		su -c "PGPASSWORD="$DB_PASS" pg_dump -U $7 -h $8 -p $9 ${10} > ${10}.bak"
-		log_errors $? "Respaldo de BD actual: ${10}.bak"
-		echo "$MIN $HOUR		*	*	$1 		root		su -c \"PGPASSWORD=\"$DB_PASS\" pg_dump -U $7 -h $8 -p $9 ${10} > $PWD/${10}`date +"%F"`.bak\"" >> $CRONTAB
-		log_errors $? "Respaldo de BD programado: $1 on $HOUR:$MIN"
+		su -c "PGPASSWORD="$DB_PASS" pg_dump -U $7 -h $8 -p $9 ${10} > $DATE-${10}.sql"
+		log_errors $? "Respaldo de BD actual: $DATE-${10}.sql"
+		echo "su -c \"PGPASSWORD=\"$DB_PASS\" pg_dump -U $7 -h $8 -p $9 ${10} > '$DATE'-${10}.sql\"" >> $CRON_SCRIPT
+		log_errors $? "Programación de respaldo de BD agregada en '$CRON_SCRIPT'"
 	fi
+
+	chown $SUDO_USER:$SUDO_USER $CURR_DIR -R
+	log_errors $? "Se asigna como dueño del directorio '$PWD' a '$SUDO_USER'"
+	echo "chown $SUDO_USER:$SUDO_USER $CURR_DIR -R" >> $CRON_SCRIPT
+	log_errors $? "Programación como dueño del directorio en '$CRON_SCRIPT'"
+	HOUR=$(echo ${11} | cut -f1 -d":")
+	MIN=$(echo ${11} | cut -f2 -d":")
+	CRONTAB="/etc/crontab"
+	chmod 700 $CRON_SCRIPT
+	# hour		min		day_of_month		month		day_of_week		user command_to_be_executed
+	# Se agrega ejecución de script para respaldos
+	echo "$MIN $HOUR		*	*	$1 		root		$CRON_SCRIPT" >> $CRONTAB
+	log_errors $? "Respaldos programados: $1 on $HOUR:$MIN"
 	if [[ $2 =~ Debian.* ]]; then systemctl restart cron; else service cron restart; fi
 	cd -
 }
