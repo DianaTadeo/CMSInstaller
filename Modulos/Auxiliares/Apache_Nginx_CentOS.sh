5f1e84a2e908f3cc85737dc5739c8d788b64cefa
RafaelVallejo <rafael.vallejo@bec.seguridad.unam.mx>
2020-02-15 13:25:54 -0600

    Se corrige instalación de git y se realiza instalación correcta de WAF para Apache

--- Modulos/Auxiliares/Apache_Nginx_CentOS.sh
+++ Modulos/Auxiliares/Apache_Nginx_CentOS.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 ## @file
 ## @author Rafael Alejandro Vallejo Fernandez
 ## @author Diana G. Tadeo Guillen
@@ -57,7 +58,7 @@ install_nginx(){
 	else
 	rpm -Uvh http://nginx.org/packages/centos/6/x86_64/RPMS/nginx-1.16.1-1.el6.ngx.x86_64.rpm
 	fi
-	
+
 	cmd="yum -y install nginx"
 	$cmd
 	log_errors $? "Instalacion de Nginx: $cmd"
@@ -79,11 +80,11 @@ install_apache_WAF(){
 	else
 	rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 	fi
-	
+
 	cmd="yum install -y mod_security mod_security_crs"
 	$cmd
 	log_errors $? "Instalando Mod Security en Apache: $cmd"
-	
+
 	sed -i "s/SecRuleEngine DetectionOnly/SecRuleEngine On/" /etc/httpd/conf.d/mod_security.conf
 	cmd="service httpd restart"
 	$cmd
@@ -93,14 +94,14 @@ install_apache_WAF(){
 	cmd="git clone https://github.com/SpiderLabs/owasp-modsecurity-crs.git"
 	$cmd
 	log_errors $? "Instalando ModSecurity en Apache: $cmd"
-	
+
 	cd owasp-modsecurity-crs
-    cp crs-setup.conf.example crs-setup.conf
-    echo "Include /etc/httpd/owasp-modsecurity-crs/crs-setup.conf" >> /etc/httpd/conf/httpd.conf
-    echo "Include /etc/httpd/owasp-modsecurity-crs/rules/*.conf" >> /etc/httpd/conf/httpd.conf
-    
-    cmd="service httpd restart"
-    $cmd
+		cp crs-setup.conf.example crs-setup.conf
+		echo "Include /etc/httpd/owasp-modsecurity-crs/crs-setup.conf" >> /etc/httpd/conf/httpd.conf
+		echo "Include /etc/httpd/owasp-modsecurity-crs/rules/*.conf" >> /etc/httpd/conf/httpd.conf
+
+		cmd="service httpd restart"
+		$cmd
 	log_errors $? "Reinicio de Apache con ModSecurity en Apache: $cmd"
 	cd $locate
 }
@@ -115,11 +116,11 @@ install_nginx_WAF(){
 	else
 	rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 	fi
-	
+
 		cmd="yum install -y mod_security mod_security_crs"
 	$cmd
 	log_errors $? "Instalando Mod Security en Apache: $cmd"
-	
+
 	#sed -i "s/locate \/ {/SecRuleEngine DetectionOnly/SecRuleEngine On/" /etc/httpd/conf.d/default.conf
 	#cmd="service httpd restart"
 	#$cmd
@@ -129,17 +130,17 @@ install_nginx_WAF(){
 	#cmd="git clone https://github.com/SpiderLabs/owasp-modsecurity-crs.git"
 	#$cmd
 	#log_errors $? "Instalando ModSecurity en Apache: $cmd"
-	
+
 	#cd owasp-modsecurity-crs
-    #cp crs-setup.conf.example crs-setup.conf
-	
-    #echo "Include /etc/httpd/owasp-modsecurity-crs/crs-setup.conf" >> /etc/nginx/conf/modSecurity.conf
-    #echo "Include /etc/httpd/owasp-modsecurity-crs/rules/*.conf" >> /etc/nginx/conf/modSecurity.conf
-    #ModSecurityEnabled on;
-    #ModSecurityConfig /usr/local/nginx/modsecurity.conf;
-    
-    #cmd="service httpd restart"
-    #$cmd
+		#cp crs-setup.conf.example crs-setup.conf
+
+		#echo "Include /etc/httpd/owasp-modsecurity-crs/crs-setup.conf" >> /etc/nginx/conf/modSecurity.conf
+		#echo "Include /etc/httpd/owasp-modsecurity-crs/rules/*.conf" >> /etc/nginx/conf/modSecurity.conf
+		#ModSecurityEnabled on;
+		#ModSecurityConfig /usr/local/nginx/modsecurity.conf;
+
+		#cmd="service httpd restart"
+		#$cmd
 	#log_errors $? "Reinicio de Apache con ModSecurity en Apache: $cmd"
 	#cd $locate
 }
@@ -150,7 +151,7 @@ echo "==============================================="
 echo "     Inicia la instalacion de $2 $3"
 echo "==============================================="
 yum update
-yum upgrade
+yum upgrade -y
 #yum install epel-release yum-utils -y
 #if [ $1 == 6 ];
 #then
@@ -160,11 +161,17 @@ yum upgrade
 #	mv /etc/httpd/conf.d/php.conf /etc/httpd/conf.d/php53.off
 #else
 #	yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-#	yum-config-manager --enable remi-php73	
+#	yum-config-manager --enable remi-php73
 #	yum  -y install php php-fpm php-common php-opcache php-mcrypt php-cli php-gd php-curl php-mysqlnd
 #	systemctl start php-fpm
 #	systemctl enable php-fpm
-#fi 
+#fi
+
+if [[ $(whereis git | cut -f2 -d':') ]]; then
+		echo $(git version)
+else
+		yum install git -y
+fi
 if [[ $2 == 'Nginx' ]];
 then
 	install_nginx $1
