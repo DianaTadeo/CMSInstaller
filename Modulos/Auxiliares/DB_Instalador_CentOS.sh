e77a5f26456c98a45e00583d79a36bcd8f6bcfed
RafaelVallejo <rafael.vallejo@bec.seguridad.unam.mx>
2020-02-19 15:29:03 -0600

    Modificación para PostgreSQL CentOS7 y validación de entradas

--- Modulos/Auxiliares/DB_Instalador_CentOS.sh
+++ Modulos/Auxiliares/DB_Instalador_CentOS.sh
@@ -53,9 +53,9 @@ install_PostgreSQL(){
 	cp /var/lib/pgsql/data/pg_hba.conf /var/lib/pgsql/data/pg_hba.conf-aux
 	sed -i "s/ident/trust/" /var/lib/pgsql/data/pg_hba.conf
 	sed -i "s/peer/trust/" /var/lib/pgsql/data/pg_hba.conf
-	sed -i "s/md5/trust/" /var/lib/pgsql/data/pg_hba.conf
+	#sed -i "s/md5/trust/" /var/lib/pgsql/data/pg_hba.conf
 	sed -i "s/Environment=PGPORT=5432/Environment=PGPORT=$2/" /lib/systemd/system/postgresql.service
-	/usr/sbin/semanage port -a -t postgresql_port_t -p tcp $2
+	[[ -f /usr/sbin/semanage ]] && /usr/sbin/semanage port -a -t postgresql_port_t -p tcp $2
 	chown postgres:postgres /var/lib/pgsql/data/pg_hba.conf
 	cmd="systemctl daemon-reload"
 	$cmd
@@ -63,12 +63,22 @@ install_PostgreSQL(){
 	cmd="systemctl restart postgresql"
 	$cmd
 	log_errors $? "Configuracion de PostgreSQL: $cmd"
-	read  -sp "Ingresa el password para ese usuario: " userPass; echo -e "\n"
+
+	while true; do
+					read -sp "Ingresa el password para el usuario '$3': " userPass; echo -e "\n"
+					if [[ -n $userPass ]]; then
+						read -sp "Ingresa nuevamente el password: " userPass2; echo -e "\n"
+						[[ "$userPass" == "$userPass2" ]] && userPass2="" && break
+						echo -e "No coinciden!\n"
+					fi
+	done
 	su postgres -c "psql -h $4 -p $2 -c 'CREATE DATABASE $1;'"
+	su postgres -c "psql -h $4 -p $2 -c 'ALTER DATABASE '$1' SET bytea_output = 'escape';'"
 	su -c "psql -h $4 -p $2 -c \"CREATE USER $3 WITH PASSWORD '$userPass'\" " postgres
 	su postgres -c "psql -h $4 -p $2 -c 'GRANT ALL PRIVILEGES ON DATABASE $1 TO $3;'"
 	rm /var/lib/pgsql/data/pg_hba.conf
 	mv /var/lib/pgsql/data/pg_hba.conf-aux /var/lib/pgsql/data/pg_hba.conf
+	sed -i "s/\(^host.*\)ident/\1md5/" /var/lib/pgsql/data/pg_hba.conf
 	chown postgres:postgres /var/lib/pgsql/data/pg_hba.conf
 				cmd="systemctl daemon-reload"
 				$cmd
@@ -112,8 +122,24 @@ install_MySQL(){
 	$cmd
 	log_errors $? "Configuracion de MySQL: $cmd [Revise archivos /etc/my.cnf y /etc/my.cnf.d/server.cnf]"
 	mysql_secure_installation
-	read -sp "Ingresa el password para el usuario '$3': " userPass; echo -e "\n"
-	read -sp "Ingresa el password para el usuario 'root' de MySQL: " rootPass; echo -e "\n"
+
+	while true; do
+					read -sp "Ingresa el password para el usuario '$3': " userPass; echo -e "\n"
+					if [[ -n $userPass ]]; then
+						read -sp "Ingresa nuevamente el password: " userPass2; echo -e "\n"
+						[[ "$userPass" == "$userPass2" ]] && userPass2="" && break
+						echo -e "No coinciden!\n"
+					fi
+	done
+
+	while true; do
+		read -sp "Ingresa el password para el usuario 'root' de MySQL: " rootPass; echo -e "\n"
+		if [[ -n $rootPass ]]; then
+			mysql -h $4 -P $2 -u root --password=$rootPass -e "\q"
+			[[ $? == '0' ]] && break
+		fi
+	done
+
 	mysql -h $4 -P $2 -u root --password=$rootPass -e "CREATE USER '$3' IDENTIFIED BY '$userPass';"
 	mysql -h $4 -P $2 -u root --password=$rootPass -e "GRANT ALL PRIVILEGES ON *.* TO $3;"
 	mysql -h $4 -P $2 -u $3 --password=$userPass -e "CREATE DATABASE $1;"
